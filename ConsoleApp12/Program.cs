9ad31aa071ca1d0cde0fdf5ddc354bba25bed950
Clever Coder <clevercoderr@gmail.com>
2021-12-21 13:42:53 +0500

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Nickname kiriting: ");
+                string nickname = Console.ReadLine();
+
+            label:
+                Console.WriteLine("Habarni kiriting: ");
+                string message = Console.ReadLine();
+
+                Console.Clear();
+
+                AddMessageToChat(nickname, message);
+                Console.WriteLine(GetChat());
+                goto label;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Habar jo'natilmadi");
+            }
+        }
+
+        static string GetChat()
+        {
+            string path = @"\\192.168.2.16\Users\Public\chat.txt";
+
+            string chat = File.ReadAllText(path);
+
+            return chat;
+        }
+
+
+        static void AddMessageToChat(string nickname, string message)
+        {
+            string path = @"\\192.168.2.16\Users\Public\chat.txt";
+            string chat = GetChat();
+            chat += nickname + ": " + message + "\n";
+            File.WriteAllText(path, chat);
+        }
+    }
+}
+
